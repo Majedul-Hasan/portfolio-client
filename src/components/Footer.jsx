986b98dc67de2b967da57956be6b7c0da309761a
Majedul Hasan <hasanmajedul@gmail.com>
2023-07-02 20:00:41 +0600

    footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className='py-2 dark:text-gray-400 text-gray-700  text-xs text-center font-light font-mono'>
+      <p>© Majedul Hasan- 2023</p>
+    </footer>
+  );
+}
