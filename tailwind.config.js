765b422f3db3545ba5e80d9d36456f7d2709a545
Majedul Hasan <hasanmajedul@gmail.com>
2023-07-02 17:30:51 +0600

    miui installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ const withMT = require('@material-tailwind/react/utils/withMT');
 
 module.exports = withMT({
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
+  resolve: {},
   darkMode: 'class',
   theme: {
     extend: {},
